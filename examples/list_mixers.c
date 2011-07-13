e9c296b80ee35d77d16946a27b903c9989a3d682
Quentin Stievenart <quentin.stievenart@gmail.com>
2011-07-13 13:48:52 +0200

    printing mixer names in list_mixer example

--- examples/list_mixers.c
+++ examples/list_mixers.c
@@ -1,9 +1,20 @@
 #include <libmix.h>
+#include <stdio.h>
 
 int main()
 {
+  int i;
+  MixMixer *mixer;
+  MixList *iterator;
   MixerAPIFD fd = mix_open_dev("/dev/mixer");
+  int n = mix_get_number_of_mixers(fd);
   MixList *mixers = mix_get_mixers(fd);
+
+  printf("Number of mixers: %d\n", n);
+  mix_foreach(iterator, mixers) {
+    mixer = iterator->data;
+    printf("Mixer %s on card %d\n", mixer->name, mixer->card_number);
+  }
 
   mix_close_dev(fd);
   return 0;
